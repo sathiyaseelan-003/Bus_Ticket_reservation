b961fd5498b157376f14cc04393ecb72030be01c
sathiyaseelan-003 <147695118+sathiyaseelan-003@users.noreply.github.com>
2025-08-03 15:27:40 +0530

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -1,9 +1,9 @@
-package busResv;
+package bus;
 
 public class Bus {
 	private int busNo;
 	private boolean ac;
-	private int capacity; //get and set
+	private int capacity; 
 	
 	Bus(int no,boolean ac,int cap){
 		this.busNo = no;
@@ -11,22 +11,22 @@ public class Bus {
 		this.capacity = cap;
 	}
 	
-	public int getBusNo(){ //accessors
+	public int getBusNo(){ 
 		return busNo;
 	}
 	
 	public boolean isAc(){
 		return ac;
 	}
-	public int getCapacity(){ //accessor method
+	public int getCapacity(){ 
 		return capacity;
 	}
 	
-	public void setAc(boolean val) { //mutators
+	public void setAc(boolean val) { 
 		ac = val;
 	}
 	
-	public void setCapacity(int cap) { //mutator
+	public void setCapacity(int cap) { 
 		capacity = cap;
 	}
 	
